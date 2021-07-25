d80faed4cfd8784b984876148fa8153219981df6
YasinVeliyev <veliyev.yasin@gmail.com>
2021-07-25 17:35:36 +0400

    new file:   NatoursApi/.gitignore 	new file:   NatoursApi/.prettierrc 	new file:   NatoursApi/app.js 	new file:   NatoursApi/dev-data/data/reviews.json 	new file:   NatoursApi/dev-data/data/tour5.js 	new file:   NatoursApi/dev-data/data/tours-simple.json 	new file:   NatoursApi/dev-data/data/tours.json 	new file:   NatoursApi/dev-data/data/users.json 	new file:   NatoursApi/dev-data/img/aarav.jpg 	new file:   NatoursApi/dev-data/img/leo.jpg 	new file:   NatoursApi/dev-data/img/monica.jpg 	new file:   NatoursApi/dev-data/img/new-tour-1.jpg 	new file:   NatoursApi/dev-data/img/new-tour-2.jpg 	new file:   NatoursApi/dev-data/img/new-tour-3.jpg 	new file:   NatoursApi/dev-data/img/new-tour-4.jpg 	new file:   NatoursApi/dev-data/templates/accountTemplate.pug 	new file:   NatoursApi/dev-data/templates/emailTemplate.pug 	new file:   NatoursApi/dev-data/templates/errorTemplate.pug 	new file:   NatoursApi/dev-data/templates/loginTemplate.pug 	new file:   NatoursApi/dev-data/templates/tourCardTemplate.pug 	new file:   NatoursApi/dev-data/templates/tourTemplate.pug 	new file:   NatoursApi/package-lock.json 	new file:   NatoursApi/package.json 	new file:   NatoursApi/public/css/style.css 	new file:   NatoursApi/public/img/favicon.png 	new file:   NatoursApi/public/img/icons.svg 	new file:   NatoursApi/public/img/logo-green-round.png 	new file:   NatoursApi/public/img/logo-green-small.png 	new file:   NatoursApi/public/img/logo-green.png 	new file:   NatoursApi/public/img/logo-white.png 	new file:   NatoursApi/public/img/pin.png 	new file:   NatoursApi/public/img/tours/tour-1-1.jpg 	new file:   NatoursApi/public/img/tours/tour-1-2.jpg 	new file:   NatoursApi/public/img/tours/tour-1-3.jpg 	new file:   NatoursApi/public/img/tours/tour-1-cover.jpg 	new file:   NatoursApi/public/img/tours/tour-2-1.jpg 	new file:   NatoursApi/public/img/tours/tour-2-2.jpg 	new file:   NatoursApi/public/img/tours/tour-2-3.jpg 	new file:   NatoursApi/public/img/tours/tour-2-cover.jpg 	new file:   NatoursApi/public/img/tours/tour-3-1.jpg 	new file:   NatoursApi/public/img/tours/tour-3-2.jpg

---
+++ NatoursApi/app.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+// app.get("/", (req, res) => {
+//     res.status(200).json({ message: "Hello form Server Side", app: "Natours" });
+// });
+
+// app.post("/", (req, res) => {
+//     res.send("Yo can post to this endpoint...");
+// });
+
+app.get("/api/v1/tours", (req, res) => {
+    res.status(200).json({
+        status: "succes",
+        data: { tours },
+    });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+    console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err, data) => {
+        if (err) {
+            return res.send(err);
+        }
+        return res.status(201).json({
+            status: "success",
+            data: {
+                tour: newTour,
+            },
+        });
+    });
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+    let tour = tours.find((tour) => tour.id == req.params.id);
+    console.log(tour);
+    if (!tour) {
+        console.log(tour);
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid Id",
+        });
+    }
+
+    return res.status(200).json({
+        status: "succes",
+        data: { tour },
+    });
+});
+
+app.patch("/api/v1/tours/:id", (req, res) => {
+    let tour = tours.find((tour) => tour.id == req.params.id);
+    if (!tour) {
+        console.log(tour);
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid Id",
+        });
+    }
+    const updatedTour = Object.assign(tour, req.body);
+    tours.push(updatedTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err, data) => {
+        if (err) {
+            return res.send(err);
+        }
+        return res.status(201).json({
+            status: "success",
+            data: {
+                tour: updatedTour,
+            },
+        });
+    });
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
